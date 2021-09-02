9cb78823e3822a44aa125334b7e77537f758f89b
denglitong <litong.deng@foxmail.com>
2021-09-02 18:31:08 +0800

    Multisets: containsAll, containsOccurrence, removeOccurrence, removeAll

--- src/main/java/com/denglitong/collection/MultisetsDemo.java
+++ src/main/java/com/denglitong/collection/MultisetsDemo.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
  * @author litong.deng@foxmail.com
  * @date 2021/9/1
  */
-public class MultisetDemo {
+public class MultisetsDemo {
 
     public static void main(String[] args) {
         String line = "twinkle twinkle little star";
@@ -49,5 +49,27 @@ public class MultisetDemo {
         LinkedHashMultiset.create();
         ConcurrentHashMultiset.create();
         ImmutableMultiset.builder();
+
+        // containsAll, containsOccurrence, removeOccurrences, removeAll
+        Multiset<String> multiset1 = HashMultiset.create();
+        multiset1.add("a", 2);
+        Multiset<String> multiset2 = HashMultiset.create();
+        multiset2.add("a", 5);
+        System.out.println(multiset1.containsAll(multiset2)); // true
+        System.out.println(Multisets.containsOccurrences(multiset1, multiset2)); // false
+        System.out.println(Multisets.containsOccurrences(multiset2, multiset1)); // true
+
+        Multisets.removeOccurrences(multiset2, multiset1);
+        System.out.println(multiset2); // [a x 3]
+        multiset2.removeAll(multiset1);
+        System.out.println(multiset2.isEmpty()); // true
+
+        // copyHighestCountFirst
+        Multiset<String> multiset = HashMultiset.create();
+        multiset.add("a", 3);
+        multiset.add("b", 5);
+        multiset.add("c", 1);
+        ImmutableMultiset<String> highestCountFirst = Multisets.copyHighestCountFirst(multiset);
+        System.out.println(highestCountFirst); // [b x 5, a x 3, c]
     }
 }
